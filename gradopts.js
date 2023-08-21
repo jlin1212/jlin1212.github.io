32690c5017f9cf2d803359d75e72d4b5a588b0b2
Jonathan Lin <jlin1212@terpmail.umd.edu>
2023-08-21 05:16:57 -0400

    add momentum. todo: tracers

--- gradopts.js
+++ gradopts.js
@@ -1,28 +1,35 @@
 class LandscapeOptimizer {
-    constructor() {}
+    constructor() {
+        this.params = {};
+    }
     apply(pos, grad) {} // Take a position and the gradient at that position, and return a new position based on internal state and gradients.
 }
 
 class VanillaSGD extends LandscapeOptimizer {
     constructor() {
         super();
-        this.lr = 1e-3;
+        this.params.lr = 1e-3;
     }
 
     apply(pos, grad) {
-        return pos.addScaledVector(grad, this.lr);
+        return pos.addScaledVector(grad, -this.params.lr);
     }
 }
 
 class Momentum extends LandscapeOptimizer {
     constructor() {
         super();
-        this.lr = 1e-3;
-        this.momentum = 1;
+        this.params.lr = 1e-3;
+        this.params.beta = 0.8;
+        this.last_grad = null;
     }
 
     apply(pos, grad) {
-        return pos + this.lr * grad;
+        let next_grad = null;
+        if (this.momentum_grad == null) next_grad = grad;
+        else next_grad = this.last_grad.lerp(grad, this.params.beta);
+        this.last_grad = next_grad;
+        return pos.addScaledVector(next_grad, -this.params.lr);
     }
 }
 
