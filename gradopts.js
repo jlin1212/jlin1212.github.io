06e975b203d5e0fdb1409484a90487d5cbabcbb0
Jonathan Lin <jlin1212@terpmail.umd.edu>
2023-08-21 03:32:22 -0400

    autopopulate params and start working on instantiation

---
+++ gradopts.js
@@ -0,0 +1,29 @@
+class LandscapeOptimizer {
+    constructor() {}
+    apply(pos, grad) {} // Take a position and the gradient at that position, and return a new position based on internal state and gradients.
+}
+
+class VanillaSGD extends LandscapeOptimizer {
+    constructor() {
+        super();
+        this.lr = 1e-3;
+    }
+
+    apply(pos, grad) {
+        return pos + this.lr * grad;
+    }
+}
+
+class Momentum extends LandscapeOptimizer {
+    constructor() {
+        super();
+        this.lr = 1e-3;
+        this.momentum = 1;
+    }
+
+    apply(pos, grad) {
+        return pos + this.lr * grad;
+    }
+}
+
+OPTIMIZERS = [VanillaSGD, Momentum];
