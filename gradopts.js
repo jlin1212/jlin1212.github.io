11f9a3650809d2a287c33c15c8ebce1b6f21cca3
Jonathan Lin <jlin1212@terpmail.umd.edu>
2023-08-21 17:09:00 -0400

    adagrad

--- gradopts.js
+++ gradopts.js
@@ -1,11 +1,11 @@
-class LandscapeOptimizer {
+class Optimizer {
     constructor() {
         this.params = {};
     }
     apply(pos, grad) {} // Take a position and the gradient at that position, and return a new position based on internal state and gradients.
 }
 
-class VanillaSGD extends LandscapeOptimizer {
+class VanillaSGD extends Optimizer {
     constructor() {
         super();
         this.params.lr = 1e-2;
@@ -16,7 +16,7 @@ class VanillaSGD extends LandscapeOptimizer {
     }
 }
 
-class Momentum extends LandscapeOptimizer {
+class Momentum extends Optimizer {
     constructor() {
         super();
         this.params.lr = 1e-2;
@@ -39,4 +39,23 @@ class Momentum extends LandscapeOptimizer {
     }
 }
 
-OPTIMIZERS = [VanillaSGD, Momentum];
+class Adagrad extends Optimizer {
+    constructor() {
+        super();
+        this.params.lr = 1e-2;
+        this.params.eps = 1e-8;
+        this.running = math.zeros(2);
+    }
+
+    apply(pos, grad) {
+        this.running = math.add(this.running, math.map(grad, math.square));
+        let avg_coeff = math.multiply(
+            -this.params.lr,
+            math.map(math.map(math.add(this.running, this.params.eps), math.sqrt), x => 1 / x)
+        )
+        let update = math.dotMultiply(avg_coeff, grad);
+        return math.add(pos, math.reshape(update, [2]));
+    }
+}
+
+OPTIMIZERS = [VanillaSGD, Momentum, Adagrad];
