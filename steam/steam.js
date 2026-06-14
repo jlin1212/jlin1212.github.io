9b0c0e85e41a09b20fab93315db8f1763351e627
Jonathan Lin <html.nj@gmail.com>
2026-06-14 18:00:17 -0400

    set up basic framework for initializing sparse operators

--- steam/steam.js
+++ steam/steam.js
@@ -2,21 +2,20 @@ import * as np from 'https://unpkg.com/numpy-ts/dist/numpy-ts.browser.js';
 
 const navier_script = `
     from pyodide.ffi import jsnull
-    from scipy.sparse import diags_array, kronsum
+    from scipy.sparse import diags_array, eye_array, kron, kronsum
     from scipy.sparse.linalg import spsolve
     import js
+
+    OPS = {}
 
     def normalize(v, vmin=None, vmax=None):
         vmin = np.amin(v) if vmin is None else vmin
         vmax = np.amax(v) if vmax is None else vmax
         return (v - vmin) / (vmax - vmin)
 
-    def solve_poisson(rhs, scale):
-        N, _ = rhs.shape
-        Ln = diags_array([np.full(N, -2), np.full(N-1, 1), np.full(N-1, 1)], offsets=[0, 1, -1])
-        Lnn = kronsum(Ln, Ln)
-        sol = spsolve(Lnn, rhs.ravel(order='F'))
-        return sol.reshape(rhs.shape, order='F')
+    def init_sim(simId, L):
+        OPS[simId] = {}
+        OPS[simId]['Dx'] = np.zeros((8, 8))
 
     def du_bar(simId, dims, L, u, s, b):
         if u is jsnull: 
@@ -31,7 +30,7 @@ const navier_script = `
         fmesh = np.meshgrid(*[fspace]*dims)
         fvecs = np.stack(fmesh, -1)
 
-        sigma = (1/4) ** 2
+        sigma = (1/2) ** 2
 
         bdists = b - fvecs[None,...]
         bdists = np.linalg.norm(bdists, axis=-1)
@@ -42,45 +41,11 @@ const navier_script = `
         F = Fmag[...,None] * np.ones((L, L, 2))
         W = 1e-5 * np.random.randn(L, L, 2)
 
-        nabla_u = np.gradient(u, 1/(L+1), axis=tuple(range(dims)))
-        nabla_u = np.array(nabla_u)
-        nabla_u = np.moveaxis(nabla_u, 0, -2)
+        u_x = u[:,:,0].ravel(order='F')
+        u_y = u[:,:,1].ravel(order='F')
 
-        advection = np.einsum('...i,...ij->...j', u, nabla_u)
-        
-        nnabla_u = np.gradient(nabla_u, 1/(L+1), axis=tuple(range(dims)))
-        nnabla_u = np.array(nnabla_u)
-        nnabla_u = np.moveaxis(nnabla_u, 0, -3)
-
-        lap_u = np.einsum('...iij->...j', nnabla_u)
-
-        nu = 0.5
-        dt = 1e-5
-        du = nu * lap_u - advection + F + W
-
-        u_new = u + dt * du
-
-        nabla_u_new = np.gradient(u_new, 1/(L+1), axis=tuple(range(dims)))
-        nabla_u_new = np.array(nabla_u_new)
-        nabla_u_new = np.moveaxis(nabla_u_new, 0, -2)
-        div_u_new = np.einsum('...ii->...', nabla_u_new)
-        div_u_new = div_u_new
-
-        p = solve_poisson(div_u_new, 1 / (L + 1))
-        print(np.amin(p), np.amax(p))
-        nabla_p = np.gradient(p, 1 / (L + 1))
-        nabla_p = np.array(nabla_p)
-        nabla_p = np.moveaxis(nabla_p, 0, -1)
-
-        u_next = u_new - dt * 40 * nabla_p
-
-        nabla_u_next = np.gradient(u_next, 1/(L+1), axis=(0,1))
-        nabla_u_next = np.array(nabla_u_next)
-        nabla_u_next = np.moveaxis(nabla_u_next, 0, -2)
-        div_u_next = np.einsum('...ii->...', nabla_u_next)
-
-        js.outputs.as_py_json()[simId].u_new = u_next
-        js.outputs.as_py_json()[simId].vis = normalize(np.atan2(u_next[:,:,1],u_next[:,:,0]))
+        js.outputs.as_py_json()[simId].u_new = np.zeros((L, L, 2))
+        js.outputs.as_py_json()[simId].vis = np.zeros((L, L))
 `;
 
 function renderArray2D(canvasId, array) {
@@ -114,14 +79,22 @@ const L = 64;
 
 function simulate(pyodide, canvasId, dims, sfunc, b) {
     outputs[canvasId] = {};
+    const locals = pyodide.toPy({ simId: canvasId, L: L });
+    pyodide.runPython("init_sim(simId, L)", { locals });
     step(pyodide, canvasId, 0, dims, sfunc, b);
 }
 
 function step(pyodide, canvasId, n, dims, sfunc, b) {
-    const locals = pyodide.toPy({ simId: canvasId, L: L, dims: dims, u: (n > 0) ? outputs[canvasId]['u_new'] : null, s: sfunc(n), b: b });
+    const locals = pyodide.toPy({ 
+        simId: canvasId, 
+        L: L, 
+        dims: dims, 
+        u: (n > 0) ? outputs[canvasId]['u_new'] : null, 
+        s: sfunc(n), b: b 
+    });
     pyodide.runPython("du_bar(simId, dims, L, u, s, b)", { locals });
     renderArray2D(canvasId, outputs[canvasId].vis.toJs());
-    if (n < 10000) setTimeout(step, 20, pyodide, canvasId, n + 1, dims, sfunc, b);
+    if (n < 100) setTimeout(step, 20, pyodide, canvasId, n + 1, dims, sfunc, b);
 }
 
 function evenBurners(dim, num) {
